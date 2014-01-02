fcd31884a12189d21b31e90ceef66f403bc6bb9b
bgokoglu <burak.gokoglu@yahoo.com>
2014-01-02 15:06:28 -0600

    initial commit

---
+++ CalendarEntry/Appointment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalendarEntry
+{
+    public class Appointment
+    {
+        public string Title { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ CalendarEntry/CalendarHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace CalendarEntry
+{
+    public class CalendarHelper
+    {
+        public static MemoryStream CreateOutlookEntry(Appointment entry)
+        {
+            return CreateIcs(entry);
+        }
+
+        public static MemoryStream CreateiCalEntry(Appointment entry)
+        {
+            return CreateIcs(entry);
+        }
+
+        public static string CreateGoogleCalendarLink(Appointment entry)
+        {
+            const string url = "https://www.google.com/calendar/render?action=TEMPLATE&hl=en&text={0}&dates={1}/{2}&location={3}&details={4}sf=true&output=xml";
+            return string.Format(url, entry.Title, GetFormattedDate(entry.StartDateTime), GetFormattedDate(entry.EndDateTime), entry.Location, entry.Description);
+        }
+
+        public static string CreateYahooCalendarLink(Appointment entry)
+        {
+            const string url = "http://calendar.yahoo.com/?v=60&view=d&type=20&title={0}&st={1}&et={2}&desc={4}&in_loc={3}";
+            return string.Format(url, entry.Title, GetFormattedDate(entry.StartDateTime), GetFormattedDate(entry.EndDateTime), entry.Location, entry.Description);
+        }
+
+        private static string GetFormattedDate(DateTime dt)
+        {
+            return dt.ToString("yyyyMMddTHHmmss");
+        }
+
+        private static MemoryStream CreateIcs(Appointment entry)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(string.Format("BEGIN:VCALENDAR{0}", Environment.NewLine));
+            sb.AppendLine(string.Format("VERSION:2.0{0}", Environment.NewLine));
+            sb.AppendLine(string.Format("PRODID:-//hacksw/handcal//NONSGML v1.0//EN{0}", Environment.NewLine));
+            sb.AppendLine(string.Format("BEGIN:VEVENT{0}", Environment.NewLine));
+            sb.AppendLine(string.Format("DTSTART:{0}{1}", GetFormattedDate(entry.StartDateTime), Environment.NewLine));
+            sb.AppendLine(string.Format("DTEND:{0}{1}", GetFormattedDate(entry.EndDateTime), Environment.NewLine));
+            sb.AppendLine(string.Format("{0}{1}", string.Format("SUMMARY:{0}", entry.Title), Environment.NewLine));
+            sb.AppendLine(string.Format("{0}{1}", string.Format("LOCATION:{0}", entry.Location), Environment.NewLine));
+            sb.AppendLine(string.Format("{0}{1}", string.Format("DESCRIPTION:{0}", entry.Description), Environment.NewLine));
+            sb.AppendLine("END:VEVENT" + Environment.NewLine);
+            sb.AppendLine("END:VCALENDAR");
+
+            var ms = new MemoryStream();
+            var enc = new UTF8Encoding();
+            var arrBytData = enc.GetBytes(sb.ToString());
+            ms.Write(arrBytData, 0, arrBytData.Length);
+            ms.Position = 0;
+
+            return ms;
+        }
+    }
+}
